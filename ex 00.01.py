61978eba59753b2d29b839b8e94e3fd250a841ce
Tsupko Alexander <tsupko.alexander@yandex.ru>
2020-07-04 19:10:59 +0300

    add corrections
    1. add comments
    2. add usage
    3. use built-in constant
    4. optimize imports

--- ex 00.01.py
+++ ex 00.01.py
@@ -1,9 +1,17 @@
-import random
-import stdio
+import math
 import sys
 
-t = float(sys.argv[1])
-P = float(sys.argv[2])
-r = float(sys.argv[3])
-e = 2.7182818284
-stdio.writeln(P * e ** ((r / 100) * t))
+import stdio
+
+t = float(sys.argv[1])  # number of years (количество лет)
+P = float(sys.argv[2])  # principal (начальная сумма денег)
+r = float(sys.argv[3])  # interest rate (проценты)
+stdio.writeln(P * math.e ** ((r / 100) * t))
+
+# Usage
+# >"ex 00.01.py" 0.5 100000 4.3
+# 102173.2790337382
+# >"ex 00.01.py" 0.5 100000 5
+# 102531.5120524429
+# >"ex 00.01.py" 0.5 100000 7.5
+# 103821.19970818251
