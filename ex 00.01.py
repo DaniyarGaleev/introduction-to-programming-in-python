83b2b26a70099d4638c8d65fb262972680ce475b
tsupko <alexander.tsupko@outlook.com>
2020-07-05 05:13:05 +0300

    add corrections
    1 add comments and usage
    2 use built-in constant math.e
    3 extract expression into variable
    4 add parentheses to make precedence explicit
    5 add rounding to two decimal places
    6 optimize imports

--- ex 00.01.py
+++ ex 00.01.py
@@ -1,9 +1,19 @@
-import random
-import stdio
+import math
 import sys
 
-t = float(sys.argv[1])
-P = float(sys.argv[2])
-r = float(sys.argv[3])
-e = 2.7182818284
-stdio.writeln(P * e ** ((r / 100) * t))
+import stdio
+
+t = float(sys.argv[1])  # number of years (количество лет)
+P = float(sys.argv[2])  # principal (начальная сумма денег)
+r = float(sys.argv[3])  # interest rate (процентная ставка)
+
+final_sum_of_money = P * (math.e ** ((r / 100) * t))
+stdio.writeln(round(final_sum_of_money, 2))
+
+# Usage (Запуск)
+# > "ex 00.01.py" 0.5 100000 4.3
+# 102173.28
+# > "ex 00.01.py" 0.5 100000 5
+# 102531.51
+# > "ex 00.01.py" 0.5 100000 7.5
+# 103821.2
